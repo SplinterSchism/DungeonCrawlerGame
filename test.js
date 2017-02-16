96aff526ac49973ec05cb55f8db5f244ec413d21
andrewEllis92 <andrewtellis92@gmail.com>
2017-02-15 20:32:42 -0500

    Crashwith

--- test.js
+++ test.js
@@ -102,6 +102,25 @@ function component(width, height, color, x, y, type) {
         this.x += this.speedX;
         this.y += this.speedY; 
     } 
+	
+	this.crashWith = function(otherobj) {
+		var myleft = this.x;
+		var myright = this.x + (this.width);
+		var mytop = this.y;
+		var mybottom = this.y + (this.height);
+		var otherleft = otherobj.x;
+		var otherright = otherobj.x + (otherobj.width);
+		var othertop = otherobj.y;
+		var otherbottom = otherobj.y + (otherobj.height);
+		var crash = true;
+		if ((mybottom < othertop) ||
+			(mytop > otherbottom) ||
+			(myright < otherleft) ||
+			(myleft > otherright)) {
+				crash = false;
+		}
+			return crash;
+	}
 }
 
 //updateGameArea: Clears screen then updates every frame.
